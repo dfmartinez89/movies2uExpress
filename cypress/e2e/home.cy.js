7d6125b3f47eb88a33a588abc80ee2d38e16ae20
Damian Ferro <dfmartinez8909@gmail.com>
2023-01-18 21:17:20 +0100

    home test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Home Test", () => {
+  it("should reach home page view", () => {
+    cy.visit("/");
+    cy.get("h1").should("contain", "Movies2uExpress");
+    cy.get("p").should("contain", "Welcome to your movies Backend");
+  });
+  it("should reach home page resource", () => {
+    cy.request("/").then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.headers["content-type"]).to.eq(
+        "text/html; charset=UTF-8"
+      );
+    });
+  });
+});
